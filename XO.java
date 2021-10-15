7a99df04020db5f5f3a015b0d37b950fd0720cf6
Raahul N <45332704+raahul42@users.noreply.github.com>
2021-10-15 11:57:13 +0530

    Update XO.java

--- XO.java
+++ XO.java
@@ -1,10 +1,5 @@
-package games;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class XO {
 
